10b3f4a0647ef3f1c52399d1f2a9d782fd0d5f34
yavuzakin <yavuzakin55@gmail.com>
2022-08-08 15:17:22 +0300

    Error handler is updated for duplicate field names

--- src/utils/globalErrorHandler.js
+++ src/utils/globalErrorHandler.js
@@ -1,6 +1,14 @@
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  return `Duplicate field value: ${value}. Please use another value!`;
+};
+
 const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+
+  if (err.code === 11000) err.message = handleDuplicateFieldsDB(err);
 
   res.status(err.statusCode).json({
     status: err.status,
